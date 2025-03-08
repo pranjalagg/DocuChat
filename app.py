c21b87d518a07a28af0ae012cc471ed2a8285c7f
Pranjal Aggarwal <54495980+pranjalagg@users.noreply.github.com>
2025-03-08 05:45:16 -0700

    Document Upload and Chunking

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from dotenv import load_dotenv
+from utils import get_pdf_text, get_text_chunks
 
 def main():
     st.set_page_config(page_title="DocuChat: Chat With Your PDFs", page_icon=":boooks:", layout="wide", initial_sidebar_state="auto")
@@ -7,8 +9,15 @@ def main():
 
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload PDFs and click on 'Process'", type=["pdf"], accept_multiple_files=True)
-        st.button("Process")
-    
+        pdf_docs = st.file_uploader("Upload PDFs and click on 'Process'", type=["pdf"], accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get text from odf
+                raw_text = get_pdf_text(pdf_docs)
+
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+
 if __name__ == "__main__":
+    load_dotenv()
     main()
---
+++ utils.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
